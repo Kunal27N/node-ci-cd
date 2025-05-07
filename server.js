82d0204e51ea394107b722b3f90e351e583f3c34
Kunal27N <114390811+Kunal27N@users.noreply.github.com>
2025-05-08 02:32:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const password = "mySuperSecret123"; // Sensitive info
+const apiKey = "AIzaSyD...";
+
 app.get('/', (req, res) => {
   res.send('Hello from Node.js App with CI/CD and SonarQube!');
 });
